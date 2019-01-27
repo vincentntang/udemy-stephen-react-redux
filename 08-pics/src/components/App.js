679440a25bd7d51ae795487ce4b11f61f905bff5
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-27 14:01:26 -0500

    93- creatingCustomClients

--- 08-pics/src/components/App.js
+++ 08-pics/src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID a9af06ad378df59bbb88d1c8acc4f624ee37fb1171fd1c0ec948c36f894f89b2"
-      }
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
     });
     this.setState({ images: response.data.results });
   };
