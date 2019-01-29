1f39f314bd083b74c24f4e61954a82b932a3f312
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-01-29 11:10:57 -0500

    148 - calling Action creators from components

--- 13-songs/src/components/SongList.js
+++ 13-songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
 
           <div classnName="content">{song.title}</div>
@@ -16,11 +22,18 @@ class SongList extends Component {
     });
   }
   render() {
+    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  {
+    selectSong
+  }
+)(SongList);
