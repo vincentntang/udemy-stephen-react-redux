4d44648fbddbdfbf07808540a8b89aa6e2599537
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-01-29 17:25:32 -0500

    159- makingRequest from actionCreator

--- 14-blog-redux-thunk/src/actions/index.js
+++ 14-blog-redux-thunk/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+  // Bad Approach!!!
+  const response = await jsonPlaceholder.get("/posts");
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: response
   };
 };
