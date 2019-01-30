782d782c167238a406d2074fe06fc2c9e15a988f
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-01-30 16:28:44 -0500

    164-redux thunk

--- 14-blog-redux-thunk/src/actions/index.js
+++ 14-blog-redux-thunk/src/actions/index.js
@@ -1,11 +1,6 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return function(dispatch, getState) {
-    const promise = jsonPlaceholder.get("/posts");
-    return {
-      type: "FETCH_POSTS",
-      payload: promise
-    };
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
