1d85f5d0bbf63bd0590fb90755ec73133238f82d
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-01-31 16:58:22 -0500

    177-fetchingSingularRecords

--- 15-blog-redux-design/src/actions/index.js
+++ 15-blog-redux-design/src/actions/index.js
@@ -4,3 +4,9 @@ export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get('/users/${id}' + id);
+  
+  dispatch({type: 'FETCH_USER', payload:response.data})
+}
