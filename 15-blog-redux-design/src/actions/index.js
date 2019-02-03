aad72f8680237007f62beb43271eaacaf65cae4b
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-02-03 00:16:58 -0500

    memoize issues

--- 15-blog-redux-design/src/actions/index.js
+++ 15-blog-redux-design/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -5,8 +6,10 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
-  const response = await jsonPlaceholder.get('/users/${id}' + id);
-  
-  dispatch({type: 'FETCH_USER', payload:response.data})
-}
+export const fetchUser = function(id) {
+  return _.memoize(async function(dispatch) {
+    const response = await jsonPlaceholder.get("/users/${id}" + id);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  });
+};
