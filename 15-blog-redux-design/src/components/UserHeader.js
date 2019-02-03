76784c83f41180beadd8f4f72ba8529caa3880b7
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-02-02 23:53:05 -0500

    180 - extractingLogicMapStateToProps

--- 15-blog-redux-design/src/components/UserHeader.js
+++ 15-blog-redux-design/src/components/UserHeader.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {fetchUser} from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId)
+    const { user } = this.props;
     if (!user) {
       return null;
     }
-    return <div className="header">{user.name}</div>
+    return <div className="header">{user.name}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return {users: state.users};
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
